fcf9ea1a440f6198b63a480373a32a5a37358d41
Angel Fernandez <angel_ferndz@live.com>
2020-08-17 03:05:52 -0400

    Create Iteration Calculator.py

---
+++ Iteration Calculator.py
@@ -0,0 +1,32 @@
+print("Iteration Calculator")
+
+initialValue = int(input("Enter Initial Value: "))
+print("---------------------------")
+print("Process [sum, sub, mul, div]")
+process = input("Enter Process: ")
+print("---------------------------")
+change = int(input("Enter Change Value: "))
+print("---------------------------")
+iterations = int(input("Enter Itterations: "))
+print()
+
+value = initialValue    
+    
+for i in range(iterations):
+    if(process == "sum"):
+        value += change
+    
+    if(process == "sub"):
+        value -= change
+        
+    if(process == "mul"):
+        value *= change
+        
+    if(process == "div"):
+        value /= change
+    
+    print("{} {}".format(i, value))
+
+print("---------------------------")
+endstr = "Total change in {} iteration(s): {} to {}".format(iterations, initialValue, value)
+print(endstr)
